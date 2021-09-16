fdeec2954e9e92af60f73fb2b8291a891331878c
madda01 <it20232368@my.sliit.lk>
2021-09-17 01:50:36 +0530

    forget password

--- app/src/main/java/com/example/dreamwedmadd/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/dreamwedmadd/ForgetPasswordActivity.java
@@ -4,38 +4,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.dreamwedmadd.database.DBConnection;
+
 public class ForgetPasswordActivity extends AppCompatActivity {
 
     Button btnreset;
+    EditText username;
+    DBConnection db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
 
-        //map the button
+        //map the elements
         btnreset=findViewById(R.id.btnreset);
+        username=findViewById(R.id.resetemailtext);
+        db=new DBConnection(this);
 
         //get intents
         Intent receiveintent= getIntent();
         String message3=receiveintent.getStringExtra("Message3");
         Toast.makeText(getApplicationContext(),message3,Toast.LENGTH_LONG).show();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
         //event handling for going to login
         btnreset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //intent creation: Explicit
-                Intent i = new Intent(ForgetPasswordActivity.this,PasswordConfirmationActivity.class);
-                startActivity(i);
+                String user= username.getText().toString();
+                Boolean checkuser= db.checkUser(user);
+                if (checkuser==true){
+                    //intent creation: Explicit
+                    Intent i = new Intent(ForgetPasswordActivity.this,PasswordConfirmationActivity.class);
+                    i.putExtra("email",user);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(ForgetPasswordActivity.this,"User does not exists",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/dreamwedmadd/PasswordConfirmationActivity.java
+++ app/src/main/java/com/example/dreamwedmadd/PasswordConfirmationActivity.java
@@ -6,30 +6,63 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dreamwedmadd.database.DBConnection;
 
 public class PasswordConfirmationActivity extends AppCompatActivity {
 
     Button btnresetpass;
+    TextView username;
+    EditText repassword,password;
+    DBConnection db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_confirmation);
 
         //mapping the button
-        btnresetpass=findViewById(R.id.btnresetlogin);
-    }
+        btnresetpass=findViewById(R.id.btnreset);
+        username=findViewById(R.id.resetemail);
+        password=findViewById(R.id.resetpass1text);
+        repassword=findViewById(R.id.resetpass2text);
 
-    @Override
-    protected void onResume() {
+        db=new DBConnection(this);
+
+        Intent i= getIntent();
+        username.setText(i.getStringExtra("username"));
+
         super.onResume();
         //event handling for login
         btnresetpass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //intent creation: Explicit
-                Intent i = new Intent(PasswordConfirmationActivity.this,LoginActivity.class);
-                i.putExtra("MessageReset","Successfully reset your password");
-                startActivity(i);
+                String user=username.getText().toString();
+                String pass=password.getText().toString();
+                String repass=repassword.getText().toString();
+
+                if(pass.isEmpty() || repass.isEmpty()){
+                    Toast.makeText(PasswordConfirmationActivity.this,"Fill all fields",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(pass.contentEquals(repass)){
+                    Boolean checkpassupdate= db.updatePassword(user,pass);
+
+                    if(checkpassupdate==true){
+                        //intent creation: Explicit
+                        Intent i = new Intent(PasswordConfirmationActivity.this,LoginActivity.class);
+                        Toast.makeText(PasswordConfirmationActivity.this,"Password updated",Toast.LENGTH_SHORT).show();
+                        startActivity(i);
+                    }else{
+                        Toast.makeText(PasswordConfirmationActivity.this,"Update not successful",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(PasswordConfirmationActivity.this,"Passwords not matching",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
