76a015e18fb1b63b172bba20a80023be5a1dda78
Madhumini Gunaratne <79505612+madda01@users.noreply.github.com>
2021-09-17 11:41:46 +0530

    Update ForgetPasswordActivity.java

--- app/src/main/java/com/example/dreamwedmadd/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/dreamwedmadd/ForgetPasswordActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.dreamwedmadd.database.DBConnection;
+
 public class ForgetPasswordActivity extends AppCompatActivity {
 
     Button btnreset;
+    EditText username;
+    DBConnection db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +24,35 @@ public class ForgetPasswordActivity extends AppCompatActivity {
 
         //map the button
         btnreset=findViewById(R.id.btnreset);
+        username=findViewById(R.id.checkemailtext);
+
+        //initialize database
+        db=new DBConnection(this);
 
         //get intents
         Intent receiveintent= getIntent();
         String message3=receiveintent.getStringExtra("Message3");
         Toast.makeText(getApplicationContext(),message3,Toast.LENGTH_LONG).show();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
         //event handling for going to login
         btnreset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //intent creation: Explicit
-                Intent i = new Intent(ForgetPasswordActivity.this,PasswordConfirmationActivity.class);
-                startActivity(i);
+                String user= username.getText().toString();
+
+                Boolean checkuser= db.checkUser(user);
+
+                if(checkuser==true){
+                    //intent creation: Explicit
+                    Intent i = new Intent(ForgetPasswordActivity.this,PasswordConfirmationActivity.class);
+                    i.putExtra("username",user);
+                    Toast.makeText(ForgetPasswordActivity.this,"User exists",Toast.LENGTH_LONG).show();
+                    startActivity(i);
+                }else{
+                    Toast.makeText(ForgetPasswordActivity.this,"User does not exists",Toast.LENGTH_LONG).show();
+                }
+
+
             }
         });
     }
