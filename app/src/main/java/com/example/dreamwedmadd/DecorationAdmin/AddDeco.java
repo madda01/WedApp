37d13b9487815ecd716c4f2fac524769b778b6ee
ThameeraFernando <sithnuwan111@gmail.com>
2021-09-19 18:36:32 +0530

    validate insert from

--- app/src/main/java/com/example/dreamwedmadd/DecorationAdmin/AddDeco.java
+++ app/src/main/java/com/example/dreamwedmadd/DecorationAdmin/AddDeco.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.dreamwedmadd.R;
 import com.example.dreamwedmadd.database.DBDecorator;
@@ -46,10 +47,30 @@ public class AddDeco extends AppCompatActivity {
                String cName=et5.getText().toString();
                String address=et6.getText().toString();
                String description=et8.getText().toString();
-               double price=Double.parseDouble(et7.getText().toString());
-               Decorator decorator=new Decorator(fName,lName,Email,Mobile,cName,description,address,price);
-               dbDecorator.addDeco(decorator);
-                startActivity(new Intent(context,AdminDecoView.class));
+               String Price=et7.getText().toString();
+
+                double price=0;
+               try {
+                   price=Double.parseDouble(Price);
+               }catch (NumberFormatException e){
+                   Toast.makeText(context, "Please enter valid price", Toast.LENGTH_SHORT).show();
+
+               }
+              
+
+
+               if (fName.equals("")||lName.equals("")||Email.equals("")||Mobile.equals("")||cName.equals("")||address.equals("")||description.equals("")||Price.equals("")){
+
+                   Toast.makeText(context, "Please enter all details", Toast.LENGTH_SHORT).show();
+               }
+                else {
+
+                   Decorator decorator=new Decorator(fName,lName,Email,Mobile,cName,description,address,price);
+                   dbDecorator.addDeco(decorator);
+                   startActivity(new Intent(context,AdminDecoView.class));
+
+               }
+
 
             }
         });
--- app/src/main/java/com/example/dreamwedmadd/database/DBDecorator.java
+++ app/src/main/java/com/example/dreamwedmadd/database/DBDecorator.java
@@ -76,4 +76,6 @@ public class DBDecorator extends SQLiteOpenHelper {
 
 
 
+
+
 }
