6adb210fc527c6c453d48250d9d2670d0d46b521
ThameeraFernando <sithnuwan111@gmail.com>
2021-09-19 19:33:12 +0530

    Retrieve Decorators

--- app/src/main/java/com/example/dreamwedmadd/database/DBDecorator.java
+++ app/src/main/java/com/example/dreamwedmadd/database/DBDecorator.java
@@ -2,12 +2,16 @@ package com.example.dreamwedmadd.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 import com.example.dreamwedmadd.models.Decorator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBDecorator extends SQLiteOpenHelper {
 
@@ -71,6 +75,37 @@ public class DBDecorator extends SQLiteOpenHelper {
 
     }
 
+    public List<Decorator> getAllDeco(){
+
+        List<Decorator> decorators=new ArrayList();
+        SQLiteDatabase db=getReadableDatabase();
+
+        String sql="SELECT * FROM "+TABLE_NAME;
+        Cursor cursor=db.rawQuery(sql,null);
+
+        if (cursor.moveToFirst()){
+            do {
+                Decorator decorator=new Decorator();
+
+                decorator.setfName(cursor.getString(1));
+                decorator.setlName(cursor.getString(2));
+                decorator.setEmail(cursor.getString(3));
+                decorator.setMobile(cursor.getString(4));
+                decorator.setcName(cursor.getString(5));
+                decorator.setAddress(cursor.getString(6));
+                decorator.setDescription(cursor.getString(7));
+                decorator.setPrice(cursor.getDouble(8));
+
+                decorators.add(decorator);
+
+
+            }while (cursor.moveToNext());
+        }
+
+
+        return decorators;
+    }
+
 
 
 
@@ -79,3 +114,31 @@ public class DBDecorator extends SQLiteOpenHelper {
 
 
 }
+
+
+//
+//    public List<ToDo> getAllToDo(){
+//
+//        List<ToDo> toDos=new ArrayList();
+//        SQLiteDatabase db=getReadableDatabase();
+//        String query="SELECT * FROM "+TABLE_NAME;
+//
+//        Cursor cursor=db.rawQuery(query,null);
+//
+//        if (cursor.moveToFirst()){
+//            do {
+//                ToDo toDo=new ToDo();
+//
+//                toDo.setId(cursor.getInt(0));
+//                toDo.setTitle(cursor.getString(1));
+//                toDo.setDescription(cursor.getString(2));
+//                toDo.setStarted(cursor.getLong(3));
+//                toDo.setFinished(cursor.getLong(4));
+//
+//                toDos.add(toDo);
+//            }while(cursor.moveToNext());
+//
+//        }
+//        return toDos;
+//
+//    }
